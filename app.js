97f97152895da24c84d55a769cb64c2154eff9d8
DannyG <dannygaines@gmail.com>
2018-06-12 23:12:35 -0700

    To-Do adds not working

--- app.js
+++ app.js
@@ -1,44 +1,21 @@
-function onReady () {
-  const addToDoForm = document.getElementById ('addToDoForm');
-  const newToDoText = document.getElementById ('newToDoText');
-  const toDoList = document.getElementById ('toDoList');
+function onReady() {
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
 
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+      title: newToDoText.value;
+      complete: false
+    });
+    newToDoText.value ='';
+  }
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    let deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<span> X </span>';
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    // empty the input
-    newToDoText.value = '';
-
-    // attach delete button to list item
-    newLi.appendChild(deleteButton);
-
-
+    createNewToDo();
   });
 }
 
 window.onload = function() {
-  onReady ();
+  onReady();
 };
