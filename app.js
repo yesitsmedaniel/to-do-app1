d567c5d3c526e85538b3f197909e8e9a80ccd50e
DannyG <dannygaines@gmail.com>
2018-06-12 23:23:23 -0700

    problem solving to-do app

--- app.js
+++ app.js
@@ -9,11 +9,31 @@ function onReady() {
       complete: false
     });
     newToDoText.value ='';
+
+    renderTheUI();
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
   }
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
+
+  renderTheUI();
 }
 
 window.onload = function() {
