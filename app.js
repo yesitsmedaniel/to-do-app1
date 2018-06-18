68eaf80a63d2b5f88319f3050f023d3b486ef823
DannyG <dannygaines@gmail.com>
2018-06-18 14:42:49 -0700

    fixed semicolon, now complete

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 function onReady() {
-  const toDos = [];
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+ const toDos = [];
+const addToDoForm = document.getElementById('addToDoForm');
 
-  function createNewToDo() {
-
-    toDos.push({
-      title: newToDoText.value;
+    function createNewToDo() {
+const newToDoText = document.getElementById('newToDoText');
+         if (!newToDoText.value) { return; }
+        toDos.push({
+      title: newToDoText.value,
       complete: false
     });
-
+        newToDoText.value = '';
+        renderTheUI();
   }
-  function renderTheUI() {
-    const toDoList = document.getElementById('toDoList');
-
-    toDoList.textContent = '';
-
-    toDos.forEach(function(toDo) {
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-  }
-  addToDoForm.addEventListener('submit', event => {
+     addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
-    newToDoText.value ='';
-    renderTheUI();
   });
+     renderTheUI();
 
+    function renderTheUI() {
+        const toDoList = document.getElementById('toDoList');
+
+        toDoList.textContent = '';
+        toDos.forEach(function(toDo) {
+              const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+            newLi.textContent = toDo.title;
+              toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+    });
+
+
+  }
 }
 
 window.onload = function() {
