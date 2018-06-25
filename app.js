3a1e1e6e040407a1a58037d531653525b5c13f01
DannyG <dannygaines@gmail.com>
2018-06-25 13:24:01 -0700

    finally got it

--- app.js
+++ app.js
@@ -1,57 +1,55 @@
 function onReady() {
   let id = 0;
-  const toDos = [];
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
 
   function createNewToDo() {
-    let newToDoText = document.getElementById('newToDoText');
+    const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
-
-
     toDos.push({
       title: newToDoText.value,
       complete: false,
-      id: id,
       id: ++id
     });
-    newToDoText.value = '';
+  }
 
-    renderTheUI();
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id !== id);
   }
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
     toDoList.textContent = '';
-    let deleteButton = document.createElement('button');
-    let deleteButText = document.createTextNode("X");
+
+
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+      checkbox.checked = toDo.complete;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '<span>Exterminate</span>';
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
       newLi.appendChild(deleteButton);
-      deleteButton.appendChild(deleteButText);
 
-      //should the deleteButton event listener be OUTside this function?
-    //  deleteButton.addEventListener('click', function())
+      deleteButton.addEventListener('click', () => {
+        toDos = deleteToDo(toDo.id);
+        renderTheUI();
+      });
     });
-
-    }                 //keep him here to close Render the Ui func
-  /* deleteButton.addEventListener('click', function (){
-     deleteToDo(toDo);
-   });*/
-
-    function deleteToDo(id) {
-      return toDos.filter(toDo => toDo.id !== id);
-    }
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+    newToDoText.value = '';
+    renderTheUI();
   });
 
   renderTheUI();
